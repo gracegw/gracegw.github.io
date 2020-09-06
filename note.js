a7ea2b4a734f61049ab5977772f4ca295a5fbef1
gracegw <55923610+gracegw@users.noreply.github.com>
2020-09-06 15:10:35 -0400

    Update note.js

--- note.js
+++ note.js
@@ -1,3 +1,19 @@
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD1kVDKLBm5jnmrmlNhxOYbpXdqGWt3pa4",
+    authDomain: "notepad-b6c82.firebaseapp.com",
+    databaseURL: "https://notepad-b6c82.firebaseio.com",
+    projectId: "notepad-b6c82",
+    storageBucket: "notepad-b6c82.appspot.com",
+    messagingSenderId: "945242521131",
+    appId: "1:945242521131:web:88cbbb4a5d690143f3f46e",
+    measurementId: "G-VDDT3W67NP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+
 function type_funct() {
   document.getElementById("text").contentEditable = true;
 }
