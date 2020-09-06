2467e5406a240f0b3d99733c3ea9448f266ad89c
gracegw <55923610+gracegw@users.noreply.github.com>
2020-09-06 15:12:22 -0400

    Update s1a.js

--- s1a.js
+++ s1a.js
@@ -1,3 +1,4 @@
+
 today = new Date();
 currentMonth = today.getMonth();
 currentYear = today.getFullYear();
@@ -107,3 +108,19 @@ function showCalendar(month, year) {
 function daysInMonth(iMonth, iYear) {
     return 32 - new Date(iYear, iMonth, 32).getDate();
 }
+
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD1kVDKLBm5jnmrmlNhxOYbpXdqGWt3pa4",
+    authDomain: "notepad-b6c82.firebaseapp.com",
+    databaseURL: "https://notepad-b6c82.firebaseio.com",
+    projectId: "notepad-b6c82",
+    storageBucket: "notepad-b6c82.appspot.com",
+    messagingSenderId: "945242521131",
+    appId: "1:945242521131:web:88cbbb4a5d690143f3f46e",
+    measurementId: "G-VDDT3W67NP"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
