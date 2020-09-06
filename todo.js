2ecf48241b7ade2344260a98b0daf9b52710b6fd
gracegw <55923610+gracegw@users.noreply.github.com>
2020-09-06 15:14:54 -0400

    Update todo.js

--- todo.js
+++ todo.js
@@ -69,3 +69,15 @@ function newElement() {
     }
   }
 }
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD1kVDKLBm5jnmrmlNhxOYbpXdqGWt3pa4",
+    authDomain: "notepad-b6c82.firebaseapp.com",
+    databaseURL: "https://notepad-b6c82.firebaseio.com",
+    projectId: "notepad-b6c82",
+    storageBucket: "notepad-b6c82.appspot.com",
+    messagingSenderId: "945242521131",
+    appId: "1:945242521131:web:88cbbb4a5d690143f3f46e",
+    measurementId: "G-VDDT3W67NP"
+  };
